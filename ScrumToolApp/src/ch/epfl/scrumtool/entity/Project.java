4d13d32c94fae20e558dec923f534357a7c87638
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2014-11-07 18:54:58 +0100

    Move forward with ProjectEditActivity Warning: never tested yet since I don't have my key yet. Also modified Project entity to add a copy constructor in the Builder

--- ScrumToolApp/src/ch/epfl/scrumtool/entity/Project.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/entity/Project.java
@@ -121,6 +121,14 @@ public final class Project implements DatabaseInteraction<Project>, Serializable
         private String description;
 
         public Builder() {
+            this.name = "";
+            this.description = "";
+        }
+        
+        public Builder(Project other) {
+            this.id = other.id;
+            this.name = other.name;
+            this.description = other.description;
         }
 
         /**
