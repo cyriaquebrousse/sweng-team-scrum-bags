4bdf3368891e5b02076f6161b630a927bb318ad5
Alexandre Veuthey <alexandre.veuthey@epfl.ch>
2014-11-07 16:50:40 +0100

    Added the method sprintEditDone to exit the sprint editing activity

--- ScrumToolApp/src/ch/epfl/scrumtool/SprintActivity.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/SprintActivity.java
@@ -1,8 +1,16 @@
 package ch.epfl.scrumtool;
 
+import ch.epfl.scrumtool.gui.components.widgets.DatePickerFragment;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * 
+ * @author AlexVeuthey
+ *
+ */
 public class SprintActivity extends Activity {
 
     @Override
@@ -11,5 +19,12 @@ public class SprintActivity extends Activity {
         setContentView(R.layout.activity_sprint);
     }
     
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
     
+    public void sprintEditDone(View v) {
+        finish();
+    }
 }
