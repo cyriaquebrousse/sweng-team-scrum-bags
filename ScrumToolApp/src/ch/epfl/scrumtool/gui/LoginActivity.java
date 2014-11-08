6493cd1715affeb0b0b0a3e62659f26b22b2e818
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2014-11-08 18:44:01 +0100

    Remove main activity (=== menu activity)

--- ScrumToolApp/src/ch/epfl/scrumtool/gui/LoginActivity.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/gui/LoginActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import ch.epfl.scrumtool.R;
@@ -17,10 +16,10 @@ import ch.epfl.scrumtool.network.GoogleSession;
  * @author Cyriaque Brousse, aschneuw, zenhaeus
  */
 public class LoginActivity extends Activity {
+    
     public static final int REQUEST_ACCOUNT_PICKER = 2;
     private GoogleSession.Builder sessionBuilder;
     private ProgressDialog pd = null;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +37,15 @@ public class LoginActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            this.pd = ProgressDialog.show(this, "Signing in..", "", true, false);
+            this.pd = ProgressDialog.show(this, "Signing in...", "", true, false);
             sessionBuilder.authenticate(data, new Callback<Boolean>() {
-
                 @Override
                 public void interactionDone(Boolean object) {
                     if (LoginActivity.this.pd != null) {
                         LoginActivity.this.pd.dismiss();
                     }
                     if (object.booleanValue()) {
-                        LoginActivity.this.openMenuActivity();
+                        LoginActivity.this.openProjectListActivityAndFinish();
                     } else {
                         Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG).show();
                     }
@@ -55,13 +53,13 @@ public class LoginActivity extends Activity {
                 }
             });
         } else {
-            Toast.makeText(this, "Please choose an Account", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Please choose an acccount", Toast.LENGTH_LONG).show();
         }
     }
 
-    public void openMenuActivity() {
-        Log.d("LoginActivity", "openMenus");
-        Intent openMenuIntent = new Intent(this, MenuActivity.class);
-        startActivity(openMenuIntent);
+    private void openProjectListActivityAndFinish() {
+        Intent openProjectListIntent = new Intent(this, ProjectListActivity.class);
+        startActivity(openProjectListIntent);
+        finish();
     }
 }
