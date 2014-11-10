69cdb8ad9458ba842f29c25bd36b3bc84737db05
Vincent Débieux <vincent.debieux@epfl.ch>
2014-11-10 16:33:14 +0100

    can acces backlog activity

--- ScrumToolApp/src/ch/epfl/scrumtool/database/google/DSProjectHandler.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/database/google/DSProjectHandler.java
@@ -142,7 +142,7 @@ public class DSProjectHandler implements ProjectHandler {
 			@Override
 			protected void onPostExecute(OperationStatus result) {
 				dbC.interactionDone(Boolean.valueOf(result.getSuccess()));
-                super.onPostExecute(result);
+				super.onPostExecute(result);
 			}
 
 		};
@@ -169,18 +169,22 @@ public class DSProjectHandler implements ProjectHandler {
 
 			@Override
 			protected void onPostExecute(CollectionResponseScrumProject result) {
-				List<ScrumProject> resultItems = result.getItems();
-				ArrayList<Project> projects = new ArrayList<Project>();
-				if (resultItems != null) {
-					for (ScrumProject sP : resultItems) {
-						Project.Builder pB = new Project.Builder();
-						pB.setDescription(sP.getDescription());
-						pB.setName(sP.getName());
-						pB.setId(sP.getKey());
-						projects.add(pB.build());
+				if (result == null) {
+					cB.interactionDone(null);
+				} else {
+					List<ScrumProject> resultItems = result.getItems();
+					ArrayList<Project> projects = new ArrayList<Project>();
+					if (resultItems != null) {
+						for (ScrumProject sP : resultItems) {
+							Project.Builder pB = new Project.Builder();
+							pB.setDescription(sP.getDescription());
+							pB.setName(sP.getName());
+							pB.setId(sP.getKey());
+							projects.add(pB.build());
+						}
 					}
+					cB.interactionDone(projects);
 				}
-				cB.interactionDone(projects);
 			}
 		};
 		task.execute();
--- ScrumToolApp/src/ch/epfl/scrumtool/gui/BacklogActivity.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/gui/BacklogActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import ch.epfl.scrumtool.R;
@@ -24,71 +25,87 @@ import ch.epfl.scrumtool.network.Client;
  * @author Cyriaque Brousse
  */
 public class BacklogActivity extends Activity {
-    
-    private ListView listView;
-    private Project project;
-    
-    private TaskListAdapter adapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_backlog);
-        
-        project = (Project) getIntent().getSerializableExtra("ch.epfl.scrumtool.PROJECT");
-        project.loadBacklog(new Callback<List<MainTask>>() {
-            
-            @Override
-            public void interactionDone(final List<MainTask> taskList) {
+	private ListView listView;
+	private Project project;
 
-                // Get list and initialize adapter
-                adapter = new TaskListAdapter(BacklogActivity.this, taskList);
-                listView = (ListView) findViewById(R.id.backlog_tasklist);
-                listView.setAdapter(adapter);
-                listView.setOnItemClickListener(new OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Intent openTaskIntent = new Intent(view.getContext(), TaskOverviewActivity.class);
-                        
-                        // Pass the task Id
-                        MainTask task = taskList.get(position);
-                        openTaskIntent.putExtra("ch.epfl.scrumtool.TASK", task);
-                        
-                        startActivity(openTaskIntent);
-                    }
-                });
-                
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-    
-    public void addTask(View view) {
-        MainTask.Builder tB = new MainTask.Builder();
-        tB.setName("Static Task");
-        tB.setDescription("I am a super super cool description... YAHOU !");
-        tB.setPriority(Priority.HIGH);
-        tB.setStatus(Status.READY_FOR_ESTIMATION);
-        tB.setId("newTaskId");
-        
-        final MainTask newTask = tB.build();
-        
-        Client.getScrumClient().insertMainTask(newTask, project, new Callback<MainTask>() {
-            
-            @Override
-            public void interactionDone(MainTask task) {
-                if (task != null) {
-                    Log.d("Task ID", task.getKey());
-                    // FIXME: need database function project.addTask(task)
-                    
-                    //Intent openTaskOverviewIntent = new Intent(BacklogActivity.this, TaskOverviewActivity.class);
-                    //openTaskOverviewIntent.putExtra("ch.epfl.scrumtool.TASK", newTask);
-                    //startActivity(openTaskOverviewIntent);
-                    
-                } else {
-                    Log.e("BacklogActivity", "Could not insert the new task");
-                }
-            }
-        });
-    }
+	private TaskListAdapter adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_backlog);
+
+		project = (Project) getIntent().getSerializableExtra(
+				"ch.epfl.scrumtool.PROJECT");
+		project.loadBacklog(new Callback<List<MainTask>>() {
+
+			@Override
+			public void interactionDone(final List<MainTask> taskList) {
+				if (taskList == null) {
+					Toast.makeText(BacklogActivity.this, "sum sing wong",
+							Toast.LENGTH_SHORT).show();
+				} else {
+
+					// Get list and initialize adapter
+					adapter = new TaskListAdapter(BacklogActivity.this,
+							taskList);
+					listView = (ListView) findViewById(R.id.backlog_tasklist);
+					listView.setAdapter(adapter);
+					listView.setOnItemClickListener(new OnItemClickListener() {
+						@Override
+						public void onItemClick(AdapterView<?> parent,
+								View view, int position, long id) {
+							Intent openTaskIntent = new Intent(view
+									.getContext(), TaskOverviewActivity.class);
+
+							// Pass the task Id
+							MainTask task = taskList.get(position);
+							openTaskIntent.putExtra("ch.epfl.scrumtool.TASK",
+									task);
+
+							startActivity(openTaskIntent);
+						}
+					});
+
+					adapter.notifyDataSetChanged();
+				}
+			}
+		});
+	}
+
+	public void addTask(View view) {
+		MainTask.Builder tB = new MainTask.Builder();
+		tB.setName("Static Task");
+		tB.setDescription("I am a super super cool description... YAHOU !");
+		tB.setPriority(Priority.HIGH);
+		tB.setStatus(Status.READY_FOR_ESTIMATION);
+		tB.setId("newTaskId");
+
+		final MainTask newTask = tB.build();
+
+		Client.getScrumClient().insertMainTask(newTask, project,
+				new Callback<MainTask>() {
+
+					@Override
+					public void interactionDone(MainTask task) {
+						if (task != null) {
+							Log.d("Task ID", task.getKey());
+							// FIXME: need database function
+							// project.addTask(task)
+
+							// Intent openTaskOverviewIntent = new
+							// Intent(BacklogActivity.this,
+							// TaskOverviewActivity.class);
+							// openTaskOverviewIntent.putExtra("ch.epfl.scrumtool.TASK",
+							// newTask);
+							// startActivity(openTaskOverviewIntent);
+
+						} else {
+							Log.e("BacklogActivity",
+									"Could not insert the new task");
+						}
+					}
+				});
+	}
 }
