bfc98d5980f449a1627707be8340603376d7e4be
Alexandre Veuthey <alexandre.veuthey@epfl.ch>
2014-11-11 17:15:26 +0100

    Added javadoc to the public methods of the DSSprintHandler.java class.

--- ScrumToolApp/src/ch/epfl/scrumtool/database/google/DSSprintHandler.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/database/google/DSSprintHandler.java
@@ -24,28 +24,39 @@ import ch.epfl.scrumtool.server.scrumtool.model.ScrumSprint;
 public class DSSprintHandler implements SprintHandler {
 
     @Override
+    /**
+     * Inserts a Sprint on the server.
+     */
     public void insert(final Sprint object, final Callback<Sprint> cB) {
         // TODO Auto-generated method stub
     }
 
     @Override
+    /**
+     * Loads a Sprint from its key (String).
+     */
     public void load(final String key, final Callback<Sprint> cB) {
-        
+        // TODO Auto-generated method stub
     }
 
     @Override
+    /**
+     * Updates the ref Sprint to be the modified Sprint.
+     */
     public void update(final Sprint modified, final Sprint ref, final Callback<Boolean> dbC) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
+    /**
+     * Removes a Sprint from the datastore.
+     */
     public void remove(Sprint object, Callback<Boolean> dbC) {
         // TODO Auto-generated method stub
-
     }
 
     /**
+     * Loads all the Sprints in the datastore.
      * @param projectKey
      * @param dbC
      */
@@ -65,7 +76,6 @@ public class DSSprintHandler implements SprintHandler {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-
                 return sprints;
             }
 
@@ -90,17 +100,18 @@ public class DSSprintHandler implements SprintHandler {
     }
 
     @Override
+    /**
+     * Inserts a Sprint in a Project.
+     */
     public void insert(Sprint sprint, Project project, Callback<Sprint> cB) {
         // TODO Auto-generated method stub
-        
     }
     @Override
+    /**
+     * Removes a Sprint from a Project.
+     */
     public void removeSprint(Sprint sprint, Project project,
             Callback<Boolean> cB) {
         // TODO Auto-generated method stub
-        
     }
-    
-    
-
 }
