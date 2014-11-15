977f072d9984640f9a904fe65e2095a6f3f57107
Arno <arno.schneuwly@epfl.ch>
2014-11-15 12:41:07 +0100

    User Handler for updated model

--- ScrumToolApp/src/ch/epfl/scrumtool/database/google/DSUserHandler.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/database/google/DSUserHandler.java
@@ -1,13 +1,17 @@
 package ch.epfl.scrumtool.database.google;
 
 import java.io.IOException;
+import java.util.Date;
 
 import android.os.AsyncTask;
 import ch.epfl.scrumtool.database.Callback;
 import ch.epfl.scrumtool.database.UserHandler;
 import ch.epfl.scrumtool.entity.User;
+import ch.epfl.scrumtool.exception.NotAuthenticatedException;
 import ch.epfl.scrumtool.network.GoogleSession;
+import ch.epfl.scrumtool.network.Session;
 import ch.epfl.scrumtool.server.scrumtool.Scrumtool;
+import ch.epfl.scrumtool.server.scrumtool.model.OperationStatus;
 import ch.epfl.scrumtool.server.scrumtool.model.ScrumUser;
 
 /**
@@ -46,11 +50,42 @@ public class DSUserHandler implements UserHandler {
             }
 
             @Override
-            protected void onPostExecute(ScrumUser scrumUser) {
-                User.Builder userBuilder = new User.Builder();
-                userBuilder.setName(scrumUser.getName());
-                userBuilder.setEmail(scrumUser.getEmail());
-                User user = userBuilder.build();
+            protected void onPostExecute(ScrumUser b) {
+                User.Builder builder= new User.Builder();
+                
+                String email = b.getEmail();
+                if (email != null) {
+                    builder.setEmail(b.getEmail());
+                }
+
+                String name = b.getName();
+                if (name != null) {
+                    builder.setName(name);
+                }
+                
+                String lastName = b.getLastName();
+                if (lastName != null) {
+                    builder.setLastName(lastName);
+                }
+
+                String companyName = b.getCompanyName();
+                if (companyName != null) {
+                    builder.setCompanyName(companyName);
+                }
+
+                String jobTitle = b.getJobTitle();
+                if (jobTitle != null) {
+                    builder.setJobTitle(jobTitle);
+                }
+                     
+                Long dateOfBirth = b.getDateOfBirth();
+                if (dateOfBirth != null) {
+                    Date date = new Date();
+                    date.setTime(dateOfBirth);
+                    builder.setDateOfBirth(date);
+                }
+
+                User user = builder.build();
                 callback.interactionDone(user);
             }
         };
@@ -63,9 +98,45 @@ public class DSUserHandler implements UserHandler {
     }
 
     @Override
-    public void update(User modified, User ref, Callback<Boolean> callback) {
-        // TODO Auto-generated method stub
+    public void update(final User user, User ref, final Callback<Boolean> callback) {
+        AsyncTask<ScrumUser, Void, OperationStatus> task = new AsyncTask<ScrumUser, Void, OperationStatus>() {
+            @Override
+            protected OperationStatus doInBackground(ScrumUser... params) {
+                OperationStatus opStat = null;
+                try {
+                    GoogleSession session = (GoogleSession) Session
+                            .getCurrentSession();
+                    params[0].setLastModUser(session.getUser().getEmail());
+                    opStat = session.getAuthServiceObject().updateScrumUser(params[0]).execute();
+                } catch (IOException | NotAuthenticatedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return opStat;
+            }
 
+            @Override
+            protected void onPostExecute(OperationStatus b) {
+                if (b != null) {
+                    callback.interactionDone(b.getSuccess());
+                } else {
+                    callback.failure("ERROR");
+                }
+            }
+        };
+        
+        ScrumUser dbUser = new ScrumUser();
+        dbUser.setCompanyName(user.getCompanyName());
+        dbUser.setDateOfBirth(user.getDateOfBirth().getTime());
+        dbUser.setJobTitle(user.getJobTitle());
+
+        dbUser.setLastModDate((new Date()).getTime());
+
+        dbUser.setEmail(user.getEmail());
+        dbUser.setLastName(user.getLastName());
+        dbUser.setName(user.getName());
+        
+        task.execute(dbUser);
     }
 
     /* (non-Javadoc)
