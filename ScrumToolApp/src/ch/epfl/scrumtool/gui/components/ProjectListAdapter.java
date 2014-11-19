0b561a3ae55a34e0b462039f73658d3f6c3d5e1d
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2014-11-19 18:26:40 +0100

    Remove new project changes sticker

--- ScrumToolApp/src/ch/epfl/scrumtool/gui/components/ProjectListAdapter.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/gui/components/ProjectListAdapter.java
@@ -10,19 +10,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import ch.epfl.scrumtool.R;
 import ch.epfl.scrumtool.entity.Project;
-import ch.epfl.scrumtool.gui.components.widgets.Sticker;
 
 /**
  * @author Cyriaque Brousse
  */
 public final class ProjectListAdapter extends DefaultAdapter<Project> {
 
-    private Activity activity;
     private LayoutInflater inflater;
     
     public ProjectListAdapter(final Activity activity, final List<Project> projectList) {
         super(projectList);
-        this.activity = activity;
         this.inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
@@ -34,22 +31,10 @@ public final class ProjectListAdapter extends DefaultAdapter<Project> {
 
         TextView name = (TextView) convertView.findViewById(R.id.project_row_name);
         TextView desc = (TextView) convertView.findViewById(R.id.project_row_description);
-        Sticker newElemCount = (Sticker) convertView.findViewById(R.id.project_row_newElemCount);
 
         Project project = getList().get(position);
         name.setText(project.getName());
         desc.setText(project.getDescription());
-        
-        // Sticker
-        int changesCount = project.getChangesCount(null);
-        newElemCount.setText(Integer.toString(changesCount));
-
-        if (changesCount == 0) {
-            newElemCount.setVisibility(View.INVISIBLE);
-        } else {
-            newElemCount.setVisibility(View.VISIBLE);
-            newElemCount.setBackgroundColor(activity.getResources().getColor(R.color.darkgreen));
-        }
 
         return convertView;
     }
