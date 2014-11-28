5bdee3874016cd488c9194211f9c6cc5c260b31a
Leonardo Wirz <leonardo.wirz@epfl.ch>
2014-11-28 16:40:08 +0100

    test that creates+removes a project on DS

--- ScrumToolAppTest/src/ch/epfl/scrumtool/gui/ProjectListActivityTest.java
+++ ScrumToolAppTest/src/ch/epfl/scrumtool/gui/ProjectListActivityTest.java
@@ -1,14 +1,18 @@
 package ch.epfl.scrumtool.gui;
 
 import ch.epfl.scrumtool.R;
+import ch.epfl.scrumtool.entity.Project;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.Menu;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.*;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.*;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.allOf;
 
 /**
  * @author LeoWirz
@@ -32,12 +36,13 @@ public class ProjectListActivityTest extends
         onView(withId(R.id.project_title_edit)).perform(typeText("espresso test"));
         onView(withId(R.id.project_description_edit)).perform(typeText("this should be removed automatically"), closeSoftKeyboard());
         onView(withId(R.id.project_edit_button_next)).check(matches(isClickable()));
+        onView(withId(R.id.project_edit_button_next)).perform(click());
         }
     
+    @SuppressWarnings("unchecked")
     public void testRemoveProject() {
-//        TODO
-//        onView(withId(R.id.project_list)).perform(longClick());
-//        onView(withId(R.id.action_entity_delete)).perform(click());
+        onData(instanceOf(Project.class)).inAdapterView(allOf(withId(R.id.project_list))).atPosition(0).perform(longClick());
+        onView(withText("Delete")).perform(click());
     }
 
 }
