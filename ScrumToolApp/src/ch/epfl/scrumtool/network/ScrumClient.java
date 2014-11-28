4519a015948851f5b3f9647e2017f812747a195e
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2014-11-28 11:55:06 +0100

    Add javadoc stubs for ScrumClient

--- ScrumToolApp/src/ch/epfl/scrumtool/network/ScrumClient.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/network/ScrumClient.java
@@ -22,43 +22,167 @@ import ch.epfl.scrumtool.entity.User;
 public interface ScrumClient {
     
     // User methods
+    /**
+     * @param user
+     * @param callback
+     */
     void deleteUser(final User user, final Callback<Boolean> callback);
+    /**
+     * @param user
+     * @param ref
+     * @param callback
+     */
     void updateUser(final User user, final User ref, final Callback<Boolean> callback);
     
     // Project methods
+    /**
+     * @param callback
+     */
     void loadProjects(final Callback<List<Project>> callback);
+    /**
+     * @param project
+     * @param callback
+     */
     void insertProject(final Project project, final Callback<Project> callback);
+    /**
+     * @param project
+     * @param ref
+     * @param callback
+     */
     void updateProject(final Project project, final Project ref, final Callback<Boolean> callback);
+    /**
+     * @param project
+     * @param callback
+     */
     void deleteProject(final Project project, final Callback<Boolean> callback);
     
     // Player methods
+    /**
+     * @param project
+     * @param callback
+     */
     void loadPlayers(final Project project, final Callback<List<Player>> callback);
+    /**
+     * @param player
+     * @param project
+     * @param callback
+     */
     void addPlayer(final Player player, final Project project, final Callback<Player> callback);
+    /**
+     * @param player
+     * @param ref
+     * @param callback
+     */
     void updatePlayer(final Player player, final Player ref, final Callback<Boolean> callback);
+    /**
+     * @param player
+     * @param callback
+     */
     void removePlayer(final Player player, final Callback<Boolean> callback);
+    /**
+     * @param project
+     * @param userEmail
+     * @param role
+     * @param callback
+     */
     void addPlayerToProject(final Project project, final String userEmail, 
             final Role role, final Callback<Player> callback);
     
     // Maintask methods
+    /**
+     * @param project
+     * @param callback
+     */
     void loadBacklog(final Project project, final Callback<List<MainTask>> callback);
+    /**
+     * @param task
+     * @param project
+     * @param callback
+     */
     void insertMainTask(final MainTask task, final Project project, final Callback<MainTask> callback);
+    /**
+     * @param task
+     * @param ref
+     * @param callback
+     */
     void updateMainTask(final MainTask task, final MainTask ref, final Callback<Boolean> callback);
+    /**
+     * @param task
+     * @param callback
+     */
     void deleteMainTask(final MainTask task, final Callback<Boolean> callback);
     
     // Issue methods
+    /**
+     * @param task
+     * @param callback
+     */
     void loadIssues(final MainTask task, final Callback<List<Issue>> callback);
+    /**
+     * @param sprint
+     * @param callback
+     */
     void loadIssues(final Sprint sprint, final Callback<List<Issue>> callback);
+    /**
+     * @param issue
+     * @param task
+     * @param callback
+     */
     void insertIssue(final Issue issue, final MainTask task, final Callback<Issue> callback);
+    /**
+     * @param issue
+     * @param sprint
+     * @param callback
+     */
     void addIssueToSprint(final Issue issue, final Sprint sprint, final Callback<Boolean> callback);
+    /**
+     * @param issue
+     * @param callback
+     */
     void removeIssueFromSprint(final Issue issue, final Callback<Boolean> callback);
+    /**
+     * @param issue
+     * @param ref
+     * @param callback
+     */
     void updateIssue(final Issue issue, final Issue ref, Callback<Boolean> callback);
+    /**
+     * @param issue
+     * @param callback
+     */
     void deleteIssue(final Issue issue, final Callback<Boolean> callback);
+    /**
+     * @param project
+     * @param callback
+     */
     void loadUnsprintedIssues(final Project project, final Callback<List<Issue>> callback);
+    /**
+     * @param user
+     * @param cB
+     */
     void loadIssuesForUser(final User user, final Callback<List<TaskIssueProject>> cB);
      
     // Sprint methods
+    /**
+     * @param project
+     * @param callback
+     */
     void loadSprints(final Project project, final Callback<List<Sprint>> callback);
+    /**
+     * @param sprint
+     * @param project
+     * @param callback
+     */
     void insertSprint(final Sprint sprint, final Project project, final Callback<Sprint> callback);
+    /**
+     * @param sprint
+     * @param ref
+     * @param callback
+     */
     void updateSprint(final Sprint sprint, final Sprint ref, final Callback<Boolean> callback);
+    /**
+     * @param sprint
+     * @param callback
+     */
     void deleteSprint(final Sprint sprint, final Callback<Boolean> callback);
 }
