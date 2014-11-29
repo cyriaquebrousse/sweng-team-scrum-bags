b4d82a9a8aacc0bfedbdfbdd282195a865e426c3
Arno Schneuwly <arno.schneuwly@epfl.ch>
2014-11-29 15:27:10 +0100

    Mockito in project

---
+++ ScrumToolAppTest/src/ch/epfl/scrumtool/database/google/DSMainTaskHandlerTest.java
@@ -0,0 +1,27 @@
+package ch.epfl.scrumtool.database.google;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DSMainTaskHandlerTest {
+    
+    
+    
+    @Before
+    public void setup() {
+       
+        
+        
+    }
+    
+    @Test
+    public void loadMainTasks() {
+        
+        
+        
+        
+        
+        
+    }
+
+}
