92fe3d87d7d7d27af926d8bccfd2e6b0164cefe0
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2014-12-03 14:13:24 +0100

    Add library to use mockito to mock our client/server database model
    There is a small example how to use mockito to mock our callbacks
    in the class ProjectListActivityTest.java but the test fails
    because it is not properly implemented.

--- ScrumToolAppTest/src/ch/epfl/scrumtool/gui/ProjectListActivityTest.java
+++ ScrumToolAppTest/src/ch/epfl/scrumtool/gui/ProjectListActivityTest.java
@@ -1,48 +1,69 @@
 package ch.epfl.scrumtool.gui;
 
-import ch.epfl.scrumtool.R;
-import ch.epfl.scrumtool.entity.Project;
-import android.test.ActivityInstrumentationTestCase2;
-import android.view.Menu;
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.*;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.*;
-import static org.hamcrest.Matchers.instanceOf;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ch.epfl.scrumtool.R;
+import ch.epfl.scrumtool.database.Callback;
+import ch.epfl.scrumtool.entity.Project;
+import ch.epfl.scrumtool.network.Client;
+import ch.epfl.scrumtool.network.DatabaseScrumClient;
 
 /**
  * @author LeoWirz
  * 
  */
-public class ProjectListActivityTest extends
-        ActivityInstrumentationTestCase2<ProjectListActivity> {
+public class ProjectListActivityTest extends ActivityInstrumentationTestCase2<ProjectListActivity> {
 
     public ProjectListActivityTest() {
         super(ProjectListActivity.class);
     }
-
+    
+    Project project = new Project.Builder()
+        .setDescription("TestDescription")
+        .setName("TestProject").build();
+    
+    List<Project> projectList = new ArrayList<Project>();
+    
+    DatabaseScrumClient mockclient = Mockito.mock(DatabaseScrumClient.class);
+    
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        Client.setScrumClient(mockclient);
+        
+        Answer<Void> answer = new Answer<Void>() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                projectList.add(project);
+                ((Callback<List<Project>>) invocation.getArguments()[0]).interactionDone(projectList);
+                return null;
+            }
+        };
+        Mockito.doAnswer(answer).when(mockclient).loadProjects(Mockito.any(Callback.class));
         getActivity();
     }
 
     public void testAddNewProject() {
-        onView(withId(Menu.FIRST)).perform(click());
-        onView(withId(R.id.project_title_edit)).perform(typeText("espresso test"));
-        onView(withId(R.id.project_description_edit)).perform(typeText("this should be removed automatically"), closeSoftKeyboard());
-        onView(withId(R.id.project_edit_button_next)).check(matches(isClickable()));
-        onView(withId(R.id.project_edit_button_next)).perform(click());
-        }
+        onData(instanceOf(Project.class)).inAdapterView(allOf(withId(R.id.project_list))).atPosition(0)
+            .check(matches(withText("TestProject")));
+    }
     
-    @SuppressWarnings("unchecked")
     public void testRemoveProject() {
-        onData(instanceOf(Project.class)).inAdapterView(allOf(withId(R.id.project_list))).atPosition(0).perform(longClick());
-        onView(withText("Delete")).perform(click());
     }
 
 }
