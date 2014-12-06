8728887d7df029e92c22b5aa00c6e128e5518eed
Sylvain Beaud <sylvain.beaud@epfl.ch>
2014-12-06 13:37:19 +0100

    Refactor the matcher withError, moved to CustomMatcher

--- ScrumToolAppTest/src/ch/epfl/scrumtool/gui/utils/CustomMatchers.java
+++ ScrumToolAppTest/src/ch/epfl/scrumtool/gui/utils/CustomMatchers.java
@@ -7,6 +7,7 @@ import org.hamcrest.TypeSafeMatcher;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.EditText;
 
 public class CustomMatchers {
 
@@ -35,4 +36,30 @@ public class CustomMatchers {
           }
         };
       }
+    
+    public final static Matcher<View> withError(final String expectedError) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof EditText)) {
+                    return false;
+                }
+                String error = "";
+                if (((EditText) view).getError() != null) {
+                     error = ((EditText) view).getError().toString();
+                } else {
+                    error = "no error";
+                }
+               
+
+                return expectedError.equals(error);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+
+        };
+    }
 }
