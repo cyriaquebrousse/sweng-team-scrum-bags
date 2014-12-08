3fb24d54ec0a7331789f2073b49ebdc8f844aca5
Arno Schneuwly <arno.schneuwly@epfl.ch>
2014-12-08 19:29:50 +0100

    VoidConverterTest

--- ScrumToolApp/src/ch/epfl/scrumtool/database/google/conversion/VoidConverter.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/database/google/conversion/VoidConverter.java
@@ -9,7 +9,6 @@ public final class VoidConverter {
 
     public  static final EntityConverter<Void, Void> VOID_TO_VOID = 
             new EntityConverter<Void, Void>() {
-
         @Override
         public Void convert(Void a) {
             return a;
