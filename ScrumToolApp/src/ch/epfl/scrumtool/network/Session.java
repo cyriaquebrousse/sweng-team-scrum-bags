fefd110f71a0377f932f007a7e2ceede670f9f33
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2014-12-10 21:47:18 +0100

    Fix logout bug, don't set currentSession=null
    in destroyCurrentSession()

--- ScrumToolApp/src/ch/epfl/scrumtool/network/Session.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/network/Session.java
@@ -67,7 +67,6 @@ public abstract class Session {
          *  Remove our user from the settings, otherwise the AccountPicker will be 
          *  skipped in the LoginActivity
          */
-        currentSession = null;
         ApplicationSettings.removeCachedUser(context);
         Intent intent = new Intent(context, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
