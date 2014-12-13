35802db8591b29d083cfd2607cc65b4d50284e70
Vincent Débieux <vincent.debieux@epfl.ch>
2014-12-13 11:36:10 +0100

    Remove Unused Exceptions All exceptions pass from server to app by the same error code (due to an AppEngine bug) Display server error message in a Toast

--- ScrumToolApp/src/ch/epfl/scrumtool/database/google/conversion/ExceptionConverter.java
+++ ScrumToolApp/src/ch/epfl/scrumtool/database/google/conversion/ExceptionConverter.java
@@ -3,9 +3,9 @@ package ch.epfl.scrumtool.database.google.conversion;
 import java.io.IOException;
 
 import ch.epfl.scrumtool.exception.NotAuthenticatedException;
-import ch.epfl.scrumtool.exception.NotFoundException;
 import ch.epfl.scrumtool.exception.ScrumToolException;
 
+import com.google.api.client.googleapis.json.GoogleJsonError;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 
 
@@ -16,25 +16,15 @@ import com.google.api.client.googleapis.json.GoogleJsonResponseException;
  */
 public final class ExceptionConverter {
 
-    public static final int BAD_REQUEST = 400;
-    public static final int UNAUTHROIZED = 401;
-    public static final int FORBIDDEN = 403;
-    public static final int NOT_FOUND = 404;
-    public static final int CONBFLICT = 409;
-    public static final int INTERNAL_SERVER_ERROR = 500;
-    public static final int SERVICE_UNAVAILABLE = 503;
-
-
     public static ScrumToolException handle(IOException e) {
         if (e instanceof GoogleJsonResponseException) {
-            switch (((GoogleJsonResponseException) e).getStatusCode()) {
-                case NOT_FOUND:
-                    return new NotFoundException(e, "Not found"); 
-                case UNAUTHROIZED:
-                    return new NotAuthenticatedException(e, "Not authenticated!");
-                default:
-            }
+            // We wanted to make a switch/case to distinguish different exceptions thrown by the server
+            // But there is a bug with this version of google app engine. We can't create our custom exceptions
+            // We can only throw one type of exception from the server
+            GoogleJsonError message = ((GoogleJsonResponseException) e).getDetails();
+            return new NotAuthenticatedException(e, message.getMessage());
+        } else {
+            return new ScrumToolException(e, "Connection error");
         }
-        return new ScrumToolException(e, "Server error");
     }
 }
